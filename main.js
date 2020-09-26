9494905d99532b7b579348691c91bad5ff043cec
itsLyLee <lyylee00@gmail.com>
2020-09-26 17:36:15 +0200

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+//Select element function
+const selectElement = function (element) {
+return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
