2c2bdaf112916fcecfacbad90ad9007953872a6a
itsLyLee <lyylee00@gmail.com>
2020-09-26 17:38:04 +0200

    update

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ return document.querySelector(element);
 
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
+
