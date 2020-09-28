456049d7fe81ace6b698b47424c10001a26c2838
itsLyLee <lyylee00@gmail.com>
2020-09-28 18:30:44 +0200

    update

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ sr.reveal(".animate-left", {
   distance: "25rem",
   delay: 300,
 });
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
